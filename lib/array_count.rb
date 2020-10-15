ee82a06bcd1ca04294a3ea5e98ff141297933784
Dena Neuman <denaweiss5@gmail.com>
2020-10-15 01:54:41 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
-  array.count do |elements|
-    elements= String
+  array.count do |element|
+    element= String
   end
 array
 end
@@ -9,7 +9,8 @@ end
 
 
 def count_empty_strings(array)
-  array.count do ||
+  array.count do |element|
+    element=" "
     end
     array
   end
